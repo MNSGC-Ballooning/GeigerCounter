e00a9ce7b4ac0affd833db2920488c8bed8cf1e9
Ryan Bowers <rbowers314@gmail.com>
2017-08-01 16:33:48 -0600

    Reworked library to support multiple GeigerCounter objects

--- GeigerCounter.cpp
+++ GeigerCounter.cpp
@@ -1,35 +1,63 @@
 #include "GeigerCounter.h"
 
-namespace Geiger {
-	unsigned long totalCount;
-	unsigned long cycleCount;
-	void geiger_ISR() {
-	totalCount++;
-	cycleCount++;
-	}
-}
+GeigerCounter** GeigerCounter::geigerList = NULL;
+byte GeigerCounter::geigerNum = 0;
 
 GeigerCounter::GeigerCounter(byte pin) {
 	this->pin = pin;
+	GeigerCounter** newGeigers = new GeigerCounter*[geigerNum + 1];
+	for (byte i = 0; i < geigerNum; i++) {
+		newGeigers[i] = geigerList[i];
+	}
+	newGeigers[geigerNum] = this;
+	geigerNum++;
+	delete[] geigerList;
+	geigerList = newGeigers;
+}
+
+GeigerCounter::~GeigerCounter() {
+	GeigerCounter** newGeigers = new GeigerCounter*[geigerNum - 1];
+	for (byte i = 0, j = 0; i < geigerNum; i++) {
+		if (!(geigerList[i] == this)) {
+			newGeigers[j] = geigerList[i];
+			j++;
+		}
+	}
+	geigerNum--;
+	delete[] geigerList;
+	geigerList = newGeigers;
 }
 
 void GeigerCounter::initialize() {
 	pinMode(pin, INPUT);
-	attachInterrupt(digitalPinToInterrupt(pin),Geiger::geiger_ISR,FALLING);
+	attachInterrupt(digitalPinToInterrupt(pin),GeigerCounter::geigerISR,FALLING);
 }
 
 unsigned long GeigerCounter::getTotalCount() {
 	noInterrupts();
-	unsigned long totalCount = Geiger::totalCount;
+	unsigned long count = totalCount;
 	interrupts();
-	return totalCount;
+	return count;
 }
 
 unsigned long GeigerCounter::getCycleCount() {
 	noInterrupts();
-	unsigned long cycleCount = Geiger::cycleCount;
-	Geiger::cycleCount = 0;
+	unsigned long count = cycleCount;
+	cycleCount = 0;
 	interrupts();
-	return cycleCount;
+	return count;
 }
 
+bool GeigerCounter::confirm() {
+	return digitalRead(pin) == LOW;
+}
+
+void GeigerCounter::geigerISR() {
+	for (byte i = 0; i < geigerNum; i++) {
+		if (geigerList[i]->confirm()) {
+			geigerList[i]->totalCount++;
+			geigerList[i]->cycleCount++;
+			break;
+		}
+	}
+}
--- GeigerCounter.h
+++ GeigerCounter.h
@@ -3,18 +3,21 @@
 
 #include <Arduino.h>
 
-namespace Geiger {
-	void geiger_ISR();
-}
-
 class GeigerCounter {
 	public:
 		GeigerCounter(byte pin);
+		~GeigerCounter();
 		void initialize();
 		unsigned long getTotalCount();
 		unsigned long getCycleCount();
+		bool confirm();
 	private:
 		byte pin;
+		volatile unsigned long totalCount;
+		volatile unsigned long cycleCount;
+		static GeigerCounter** geigerList;
+		static byte geigerNum;
+		static void geigerISR();
 };
 
 #endif
