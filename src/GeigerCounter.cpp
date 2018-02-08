1e2bd597d364546ab0d41466856d5a0a641cd4ff
Ryan Bowers <rbowers314@gmail.com>
2018-02-08 16:25:44 -0600

    Replaced tabs with spaces for neatness on Github

--- src/GeigerCounter.cpp
+++ src/GeigerCounter.cpp
@@ -6,66 +6,66 @@ byte GeigerCounter::geigerNum = 0;
 
 //constructor for GeigerCounter object. Adds newly created object to array
 GeigerCounter::GeigerCounter(byte pin):
-	pin(pin) {
-	GeigerCounter** newGeigers = new GeigerCounter*[geigerNum + 1];
-	for (byte i = 0; i < geigerNum; i++) {
-		newGeigers[i] = geigerList[i];
-	}
-	newGeigers[geigerNum] = this;
-	geigerNum++;
-	delete[] geigerList;
-	geigerList = newGeigers;
+  pin(pin) {
+  GeigerCounter** newGeigers = new GeigerCounter*[geigerNum + 1];
+  for (byte i = 0; i < geigerNum; i++) {
+    newGeigers[i] = geigerList[i];
+  }
+  newGeigers[geigerNum] = this;
+  geigerNum++;
+  delete[] geigerList;
+  geigerList = newGeigers;
 }
 
 //explicit destructor to handle removing object from array. Hopefully will never actually be needed
 GeigerCounter::~GeigerCounter() {
-	GeigerCounter** newGeigers = new GeigerCounter*[geigerNum - 1];
-	for (byte i = 0, j = 0; i < geigerNum; i++) {
-		if (!(geigerList[i] == this)) {
-			newGeigers[j] = geigerList[i];
-			j++;
-		}
-	}
-	geigerNum--;
-	delete[] geigerList;
-	geigerList = newGeigers;
+  GeigerCounter** newGeigers = new GeigerCounter*[geigerNum - 1];
+  for (byte i = 0, j = 0; i < geigerNum; i++) {
+    if (!(geigerList[i] == this)) {
+      newGeigers[j] = geigerList[i];
+      j++;
+    }
+  }
+  geigerNum--;
+  delete[] geigerList;
+  geigerList = newGeigers;
 }
 
 //call during setup to connect the ISR to the geiger's pin and start tracking hits
 void GeigerCounter::initialize() {
-	pinMode(pin, INPUT);
-	attachInterrupt(digitalPinToInterrupt(pin),GeigerCounter::geigerISR,FALLING);
+  pinMode(pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(pin),GeigerCounter::geigerISR,FALLING);
 }
 
 //returns the total number of hits the geiger has detected since call to initialize()
 unsigned long GeigerCounter::getTotalCount() {
-	noInterrupts();
-	unsigned long count = totalCount;
-	interrupts();
-	return count;
+  noInterrupts();
+  unsigned long count = totalCount;
+  interrupts();
+  return count;
 }
 
 //returns the number of hits since the last time this function was called. Useful for hits/unit time
 unsigned long GeigerCounter::getCycleCount() {
-	noInterrupts();
-	unsigned long count = cycleCount;
-	cycleCount = 0;
-	interrupts();
-	return count;
+  noInterrupts();
+  unsigned long count = cycleCount;
+  cycleCount = 0;
+  interrupts();
+  return count;
 }
 
 //called in ISR to determine which geiger triggered the interrupt
 bool GeigerCounter::confirm() {
-	return digitalRead(pin) == LOW;
+  return digitalRead(pin) == LOW;
 }
 
 //static ISR to find and increment the correct geiger's counter variables when interrupt is triggered
 void GeigerCounter::geigerISR() {
-	for (byte i = 0; i < geigerNum; i++) {
-		if (geigerList[i]->confirm()) {
-			geigerList[i]->totalCount++;
-			geigerList[i]->cycleCount++;
-			break;
-		}
-	}
+  for (byte i = 0; i < geigerNum; i++) {
+    if (geigerList[i]->confirm()) {
+      geigerList[i]->totalCount++;
+      geigerList[i]->cycleCount++;
+      break;
+    }
+  }
 }
--- src/GeigerCounter.h
+++ src/GeigerCounter.h
@@ -4,20 +4,20 @@
 #include <Arduino.h>
 
 class GeigerCounter {
-	public:
-		GeigerCounter(byte pin);
-		~GeigerCounter();
-		void initialize();
-		unsigned long getTotalCount();
-		unsigned long getCycleCount();
-	private:
-		byte pin;
-		volatile unsigned long totalCount;
-		volatile unsigned long cycleCount;
-		bool confirm();
-		static GeigerCounter** geigerList;
-		static byte geigerNum;
-		static void geigerISR();
+  public:
+    GeigerCounter(byte pin);
+    ~GeigerCounter();
+    void initialize();
+    unsigned long getTotalCount();
+    unsigned long getCycleCount();
+  private:
+    byte pin;
+    volatile unsigned long totalCount;
+    volatile unsigned long cycleCount;
+    bool confirm();
+    static GeigerCounter** geigerList;
+    static byte geigerNum;
+    static void geigerISR();
 };
 
 #endif
